bb849db815757ee2ba1406581eaa575496ceb203
Abhishek Dhapare <abhishekdhapare@Abhisheks-MacBook-Pro.local>
2025-02-21 19:10:05 +0530

    localstorage implimentation

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ saveButton.addEventListener("click", () => {
     }, 5000);
     return;
   }
+
+  // let cardTitleList = document.cookie.split("=")[1];  // --- Cookies - get cookie
+  let cardTitleList = localStorage.getItem("notes");   // Local storage - get item from local storage
+  if (cardTitleList == null) {
+    cardTitleList = [
+      {
+        title: cardTitle,
+      },
+    ];
+  } else {
+    cardTitleList = JSON.parse(cardTitleList);
+    cardTitleList.push({
+      title: cardTitle,
+    });
+  }
+
+  // document.cookie = "notes=" + JSON.stringify(cardTitleList); // --- Cookies - set cookie
+  localStorage.setItem("notes", JSON.stringify(cardTitleList)); // Local storage - set item in local storage
   let noteCard = `
           <div class="card w-96 shadow-lg p-4 mt-2">
               <div class="card-body flex flex-row justify-between items-center gap-2">
@@ -44,6 +62,17 @@ saveButton.addEventListener("click", () => {
 notesContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-note")) {
     e.target.parentElement.parentElement.remove();
+    // let cardTitle = document.cookie.split("=")[1]; // --- Cookies - get cookie
+    let cardTitle = localStorage.getItem("notes"); // Local storage - get item from local storage
+    cardTitle = JSON.parse(cardTitle);
+    cardTitle.forEach((element, index) => {
+      // console.log(e.target.previousElementSibling.innerText);  
+      if (element.title == e.target.previousElementSibling.innerText) {
+        cardTitle.splice(index, 1);
+        // document.cookie = "notes=" + JSON.stringify(cardTitle); // --- Cookies - set cookie
+         localStorage.setItem("notes", JSON.stringify(cardTitle)); // Local storage - set item in local storage
+      }
+    });
     // console.log(e.target.parentElement.parentElement);
   }
 });
@@ -53,5 +82,25 @@ document.addEventListener("DOMContentLoaded", () => {
   //   console.log(cards.length);
   if (cards.length === 0) {
     notesContainer.innerHTML = "<h1 class='text-2xl '>No Notes Available</h1>";
+  }
+
+  // console.log(document.cookie);
+  // let cardTitle = document.cookie.split("=")[1];
+  let cardTitle = localStorage.getItem("notes");
+  if (cardTitle != null) {
+    // console.log("ddd");
+    cardTitle = JSON.parse(cardTitle);
+    console.log(cardTitle);
+    cardTitle.forEach((element) => {
+      let noteCard = `
+          <div class="card w-96 shadow-lg p-4 mt-2">
+              <div class="card-body flex flex-row justify-between items-center gap-2">
+                  <h2 class="card-title">${element.title}</h2>
+                  <i class="fa-solid fa-trash text-red-600 delete-note"></i>
+              </div>
+          </div>
+          `;
+      notesContainer.innerHTML += noteCard;
+    });
   }
 });
